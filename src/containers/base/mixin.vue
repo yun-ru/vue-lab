c1461cf47f7c1ddbdc777458b1a16982158be8f8
myblue <mp4bj6ruby@gmail.com>
2016-09-03 07:17:30 +0800

    s06 temp

---
+++ src/containers/base/mixin.vue
@@ -0,0 +1,35 @@
+<template>
+    <h3>Mixin</h3>
+    <button @click="sayHi">sayHi</button>
+</template>
+
+<script>
+    var myMixin = {
+        created() {
+            console.log("I been created from remote mixin!")
+        },
+        methods: {
+            sayHi() {
+                alert("hi from remote mixin")
+            }
+        }
+    };
+    export default {
+        mixins: [myMixin],
+        created() {
+            console.log("I been created from component self!")
+        },
+        data() {
+            return {
+
+            }
+        },
+        methods: {
+            sayHi() {
+                alert("hi")
+            }
+        }
+    }
+
+</script>
+
--- src/containers/s06.vue
+++ src/containers/s06.vue
@@ -1,13 +1,57 @@
 <template>
     <h1>{{$route.name}}</h1>
+    <div>
+        <button @click="greet">greet</button>
+        <button @click="say('hi ruby')">say</button>
+        <button @click="showMe('cool',$event)">showMe</button>
+    </div>
+    <br>
+    <div @click="doThis">
+        <p><a @click.prevent="doThis" href="#">prevent</a></p>
+        <p><a @click.stop="doThis" href="#">stop</a></p>
+        <p><a @click.stop.prevent="doThis" href="#">prevent & stop</a></p>
+    </div>
+    <br>
+    <div>
+        <input type="text" @keyup.13="handleSubmit">
+        <input type="text" @keyup.enter="handleSubmit">
+    </div>
 
 </template>
 
 <script>
     export default {
-        
+        data() {
+            return {
+                msg: "Have a good day!"
+            }
+        },
+        methods: {
+            greet(event) {
+                alert(this.msg);
+                console.log(event.target.tagName)
+            },
+            say(msg) {
+                alert(msg)
+            },
+            showMe(msg,event) {
+                event.target.className = msg
+            },
+            doThis() {
+                alert("Do this!")
+            },
+            handleSubmit(event) {
+                alert("You send " + event.target.value)
+            }
+        }
     }
 
 </script>
+
+<style lang="stylus">
+    button.cool
+        background-color: pink
+
+</style>
 
 
